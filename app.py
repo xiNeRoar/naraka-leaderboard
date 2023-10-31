fbba6ec820de8d9bbc8f0ae8ba88d15e33bba5e9
xiNeRoar <66216186+xiNeRoar@users.noreply.github.com>
2023-10-31 15:05:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import json
+from flask import Flask, render_template, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # for flash messages
 
-# Sample data
-players = [
-    {"rank": 1, "name": "Player A", "score": 5000},
-    {"rank": 2, "name": "Player B", "score": 4800},
-    {"rank": 3, "name": "Player C", "score": 4700},
-    {"rank": 4, "name": "Player D", "score": 4600}
-]
+def init_db():
+    with sqlite3.connect('leaderboard.db') as conn:
+        conn.execute("""
+        CREATE TABLE IF NOT EXISTS players (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            score INTEGER NOT NULL
+        );
+        """)
+        conn.execute("""
+        CREATE TABLE IF NOT EXISTS rules (
+            id INTEGER PRIMARY KEY,
+            rule TEXT NOT NULL
+        );
+        """)
 
 @app.route('/')
 def index():
-    return render_template('index.html', players=players)
+    with sqlite3.connect('leaderboard.db') as conn:
+        players = conn.execute("SELECT * FROM players ORDER BY score DESC").fetchall()
+        rules = conn.execute("SELECT * FROM rules").fetchall()
+    return render_template('index.html', players=players, rules=rules)
 
 @app.route('/update', methods=['POST'])
 def update():
-    # Get data from form
-    name = request.form.get('name')
-    score = request.form.get('score')
-    # Update player data (for simplicity, we'll just append)
-    players.append({"rank": len(players) + 1, "name": name, "score": int(score)})
-    flash("Player updated successfully!")
-    return redirect(url_for('index'))
+    data = request.json
+    with sqlite3.connect('leaderboard.db') as conn:
+        if 'player' in data:
+            conn.execute("INSERT INTO players (name, score) VALUES (?, ?)", (data['player']['name'], data['player']['score']))
+        if 'rule' in data:
+            conn.execute("INSERT INTO rules (rule) VALUES (?)", (data['rule'],))
+    return jsonify(success=True)
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
